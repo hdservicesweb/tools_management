e11bb6b15d315dd8ea1a47cdcd2b3a512e2351fe
Miguel Matus <mmatus585@gmail.com>
2019-12-03 12:36:58 -0800

    Starting proyect

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php 
+
+$host = $_SERVER["HTTP_HOST"];
+$host_user = $_SERVER['REMOTE_ADDR'];
+echo $host;
+echo "<br>";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <script language="JavaScript"> function GetLocalIPAddr(){ var oSetting = null; var ip = null; try{ oSetting = new ActiveXObject("rcbdyctl.Setting"); ip = oSetting.GetIPAddress; if (ip.length == 0){ return "Not connected Internet"; } oSetting = null; }catch(e){ return ip; } return ip; } document.write(GetLocalIPAddr()+"<br/>") </script> 
+
+    </body>
+</html>
